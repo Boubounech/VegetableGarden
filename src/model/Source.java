329d78b3a1155847cf8c1460a55988e26cb4ce29
florianBurdairon <florian.burdairon@gmail.com>
2023-05-15 10:54:11 +0200

    Add generic source - Changes by boune

--- src/model/Source.java
+++ src/model/Source.java
@@ -1,4 +1,35 @@
 package model;
 
-public interface Source {
+/**
+ *  - strength : int
+ *  - length : int
+ *
+ *  + SetSourceData(str:int, len:int)
+ *  + GetStrength() : int
+ *  + GetLength() : int
+ */
+public abstract class Source {
+    private int strength;
+    private int length;
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setSourceData(int strength, int length) {
+        setStrength(strength);
+        setLength(length);
+    }
 }
